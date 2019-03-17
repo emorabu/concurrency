f23a04bfeebe6b5a810e768cd6980d875e2fe1cb
emorabu <emorabu@gmail.com>
2019-03-17 17:47:28 +0800

    添加日志

--- src/main/java/com/aironi/concurrency/ConcurrencyTest.java
+++ src/main/java/com/aironi/concurrency/ConcurrencyTest.java
@@ -5,13 +5,17 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.aironi.concurrency.annotations.ThreadUnsafe;
 
 @ThreadUnsafe
 public class ConcurrencyTest {
 	public static int clientTotal = 5000; // 请求总数
 	public static int threadTotal = 200; // 并发线程数
-	public static int count = 0; // 技数器
+	public static int count = 0; // 计数器
+	private static final Logger LOGGER = LoggerFactory.getLogger(ConcurrencyTest.class);
 
 	public static void main(String[] args) throws Exception {
 		ExecutorService threadPool = Executors.newCachedThreadPool(); // 创建线程池
@@ -23,15 +27,15 @@ public class ConcurrencyTest {
 					semaphore.acquire();
 					add();
 					semaphore.release();
-				}catch (Exception e) {
-					System.out.println("ConcurrencyTest.main()" + e.getMessage());
+				} catch (Exception e) {
+					LOGGER.debug(e.getMessage());
 				}
 				countDownLatch.countDown();
 			});
 		}
 		countDownLatch.await();
 		threadPool.shutdown();
-		System.out.println("ConcurrencyTest.main() "+ count); // 每次输出的值都不同, 线程不安全
+		LOGGER.info("count:" + count); // 每次输出的值都不同, 线程不安全
 	}
 
 	private static void add() {
